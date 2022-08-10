51e233f935357a50c4be142641783e6cc2fde4a3
Ryan Gregory <ryan.middleFiddle@gmail.com>
2022-08-09 22:25:16 -0400

    expected #7 -#9 to pass

--- src/App.js
+++ src/App.js
@@ -1,79 +1,126 @@
-import { useEffect, useState } from "react"
-import Button from "./Button"
+import { useState } from "react"
 import { nanoid } from "nanoid"
 
+const compute = (p, c, op) => {
+  const [prev, curr] = [Number(p), Number(c)]
+  console.log(prev, curr)
+  switch (op) {
+    case "add":
+      return `${prev + curr}`
+
+    case "subtract":
+      return `${prev - curr}`
+    case "multiply":
+      return `${prev * curr}`
+    case "divide":
+      return `${prev / curr}`
+
+    default:
+      break;
+  }
+}
+
+const Display = ({ display }) => {
+  return (
+
+    <div id="display">
+      {display}
+    </div>
+  )
+}
+
+const Button = ({ id, handleInput, value, handleOperator, isInput }) => {
+  return (
 
 
-function App() {
-  const [currentInput, setCurrentInput] = useState(0)
+    <div className={id}>
+      <button type="button" id={id} onClick={isInput ? handleInput : handleOperator}>{value}</button>
+    </div>
+
+
+  )
+}
+
+const operatorMap = (str) => {
+  let result;
+  // eslint-disable-next-line default-case
+  switch (str) {
+    case "add": result = "+"
+      break
+    case "subtract": result = "-"
+      break
+    case "multiply": result = "*"
+      break
+    case "divide": result = "/"
+      break
+    case "equals": result = "="
+      break
+    case "clear": result = "C"
+      break
+  }
+  return result
+}
+
+const App = () => {
+  const [prevInput, setPrevInput] = useState('0')
   const [activeOp, setActiveOp] = useState('')
-  const [prevInput, setPrevInput] = useState('')
-  const [display, setDisplay] = useState(currentInput)
-
+  const [display, setDisplay] = useState('0')
+  const [opLast, setOpLast] = useState(false)
 
   // use enums for state variable?
   const operators = ["add", "subtract", "multiply", "divide", "equals", "clear"]
   const inputs = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "decimal"]
-  console.log(prevInput, activeOp, display)
+  //console.log(prevInput, activeOp, display)
 
   const handleInput = (e) => {
-    setDisplay(prev => prev + e.target.innerText)
-  }
+    setPrevInput(display)
 
-  const compute = (p, c, op) => {
-    const [prev, curr] = [Number(p), Number(c)]
-    switch (op) {
-      case "+":
-        return prev + curr
-
-      case "-":
-        return prev - curr
-      case "*":
-        return prev * curr
-      case "/":
-        return prev / curr
-      default:
-        break;
+    if (display === '0' || opLast) {
+      setOpLast(false)
+      setDisplay(e.target.innerText)
+      return
     }
+
+    setOpLast(false)
+    setDisplay(prev => prev + e.target.innerText)
+
   }
+
 
   const handleOperator = (e) => {
-    if (e.target.innerText === "C") {
-      setDisplay(0)
-      setPrevInput(0)
+
+
+    setOpLast(true)
+
+    if (e.target.id === "clear") {
+      console.log("clear")
+      setDisplay('0')
+      setPrevInput('0')
+      setActiveOp('')
+      setOpLast(false)
+      return
+    }
+
+    setActiveOp(e.target.id)
+
+    if (e.target.id === "equals") {
+      setPrevInput('0')
+      setDisplay(compute(prevInput, display, activeOp))
+
       setActiveOp('')
       return
     }
 
-    if (e.target.innerText === "=") {
-      setDisplay(compute(prevInput, display, activeOp))
+    if (activeOp !== '') {
+      setPrevInput(prevInput => compute(prevInput, display, activeOp))
+      setDisplay(display => compute(prevInput, display, activeOp))
       return
     }
 
-    setPrevInput(display)
-    setActiveOp(e.target.innerText)
+
   }
 
 
-  const operatorMap = (str) => {
-    let result;
-    // eslint-disable-next-line default-case
-    switch (str) {
-      case "add": result = "+"
-        break
-      case "subtract": result = "-"
-        break
-      case "multiply": result = "*"
-        break
-      case "divide": result = "/"
-        break
-      case "equals": result = "="
-        break
-      case "clear": result = "C"
-        break
-    }
-    return result
-  }
 
   const buttonMaker = (arrId, type) => {
     const isInput = type === "inputs";
@@ -86,20 +133,14 @@ function App() {
     return elements
   }
 
+  console.log([prevInput, activeOp, display])
+  console.log(typeof prevInput, typeof activeOp, typeof display)
 
   return (
     <div className="container">
-      <div div className="display" > {display}</div>
-
+      <Display display={display} />
       {buttonMaker(inputs, "inputs")}
-
-
-
       {buttonMaker(operators, "operators")}
-
-
-
-
     </div >
   );
 }
